f686a071d4ca61ae51d5edc0aebf830c5f627b05
David O'Grady <dmo7@njit.edu>
2015-05-07 06:53:14 -0400

    making some changes

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
 	<head>
-		<title>MySQL Queries</title>
+		<title>College Information</title>
 		<link rel="stylesheet" href="style.css">
 	</head>
 <body>
@@ -17,21 +17,22 @@
 		$qry = new $_GET['page'];
 	} else {
 
-	echo "<h2> David O'Grady IS218 HW3 MySQL Queries</h2>";
-	echo "<h2> Please select a question below or insert or edit entry:</h2>";
+	echo "<h1>Integrated Post Secondary Educational Data System</h1>";
+	echo "<h2> David O'Grady IS218 Project 2</h2>";
+	echo "<h2> Please select a question below for more information:</h2>";
 	echo '<table id="list1">';
-	echo '<tr><th><a href="?page=page1"> Who is the highest paid employee?</a></th></tr>';
-	echo '<tr><th><a href="?page=page2"> Who is the highest paid employee between 1985 and 1981?</a></th></tr>';
-	echo '<tr><th><a href="?page=page3"> Which department currently has highest paid department manager?</a></th></tr>';
-	echo '<tr><th><a href="?page=page4"> What are the titles of all the departments?</a></th></tr>';
-	echo '<tr><th><a href="?page=page5"> Who are the current department heads?</a></th></tr>';
-	echo '<tr><th><a href="?page=page6"> Who is highest paid employee that is not a department head?</a></th></tr>';
-	echo '<tr><th><a href="?page=page7"> Who is currently the lowest paid employee?</a></th></tr>';
-	echo '<tr><th><a href="?page=page8"> How many employees currently work in each department?</a></th></tr>';
-	echo '<tr><th><a href="?page=page9"> How much does each department currently spend on salaries?</a></th></tr>';
-	echo '<tr><th><a href="?page=page10"> How much is currently spent for all salaries?</a></th></tr>';
-	echo '<tr><th><a href="?page=pageinsert">Insert entry.</a></th></tr>';
-	echo '<tr><th><a href="?page=pageupdate">Update entry.</a></th></tr>';
+	echo '<tr><th><a href="?page=page1"> 1. Colleges with the highest percentage of women students</a></th></tr>';
+	echo '<tr><th><a href="?page=page2"> 2. Colleges with the highest percentage of male students</a></th></tr>';
+	echo '<tr><th><a href="?page=page3"> 3. Colleges with the largest endowment overall</a></th></tr>';
+	echo '<tr><th><a href="?page=page4"> 4. Colleges with the largest enrollment of freshman</a></th></tr>';
+	echo '<tr><th><a href="?page=page5"> 5. Colleges with the lowest non zero tuition revenue</a></th></tr>';
+	echo '<tr><th><a href="?page=page6"> 6. Colleges with the lowest non zero tuition revenue</a></th></tr>';
+	echo '<tr><th><a href="?page=page7"> 7. The top 10 colleges by region for the following statistics:</a><br>
+						<ol id="list3"><li> Endowment </li>
+						    <li> Total Curent Assets </li>
+						    <li> Total Current Liabilities </li>
+						    <li> Lowest Non-Zero Tuition </li>
+						    <li> Highest Tuition </li></ol></th></tr>';
 	echo "</table>";
 	}
 ?>
